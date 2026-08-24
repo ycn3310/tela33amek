6a82ddbc385242a4a10cf1660c328c82c47417ad
ycn3310 <ahmedyacine.akouchi@gmail.com>
2026-08-24 23:51:40 +0100

    changed courses logo's location from cloudflare the website repo

--- tela33amek/settings.py
+++ tela33amek/settings.py
@@ -106,8 +106,7 @@ STATICFILES_STORAGE = (
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    ".railway.app",
-    'tela33amek-nwp2f0opv-ycn3310s-projects.vercel.app',
+    'tela33amek-2vcidzdr4-ycn3310s-projects.vercel.app',
     'tela33amek.vercel.app',
     '192.168.0.151',
 ]
